4b213c331f6e73f243a866ff2cd77633a06d2c58
Torsten <tspindler@gmail.com>
2014-08-15 08:59:47 +0100

    Initial release
    	- working app on localhost:3000/usfl
    	- uses mongodb backend

---
+++ usfl/public/javascripts/players.js
@@ -0,0 +1,107 @@
+$(document).ready(function() {
+  $( "#dialog" ).dialog({ width:350 }, { autoOpen: false }, { position: { my: "left top", at: "right+20 top" } })
+	        .css("font-size", "80%");;
+  var playerInfo;
+  var playerStats;
+  $.ajax({
+    type: "GET",
+    url: "http://usflga.me:3000/player/2035",
+    dataType: "json",
+    success: function(data) {console.log("got data"); getPlayers(data)}
+  });
+});
+
+function getPlayers(data) {
+  data.forEach(function(playerId) {
+    $.ajax({
+        type: "GET",
+        url: "http://usflga.me:3000/playerid/" + playerId,
+        dataType: "json",
+        success: function(data) {markPlayer(data)}
+     });
+  });
+  $( "#ready" ).html("<p>Ready with player information</p>");
+}
+
+function markPlayer(player) {
+  var id = player["Player_ID"];
+  var name = player["First_Name"] + " " + player["Last_Name"];
+  var pos = player["Position"];
+  // console.log("marking player " + name);
+  $( "font" ).filter(function() {
+    return $(this).text().indexOf(name) > -1;
+  }).closest("font").click(function() {showInfo(id, name, pos, player)});
+}
+
+function stats(player) {
+  var first = player["First_Name"];
+  var last = player["Last_Name"];
+  $.ajax({
+    type: "GET",
+    url: "http://usflga.me:3000/player/" + first + "/" + last,
+    dataType: "json",
+    async: false,
+    success: function(data) {showStat(data)}
+  });
+}
+
+function showStat(player) {
+  var retStat = "<table cellpadding='4'><col width='200'><col width='100'>";
+  for (var prop in player.ratings) {
+    if ( (player.ratings[prop] != "0") &&
+         (prop != "_id") &&
+         (prop != "coll") &&
+         (prop != "line") &&
+         (prop != "Record_Number") ) {
+      var cleanProp = prop.replace("_", " ");
+      retStat += "<tr><td>" + cleanProp + ":</td><td align='right'>" + player.ratings[prop] + "</td></tr>\n";
+    }
+  }
+  retStat += "</table>";
+  $( "#tab1" ).html(retStat);
+
+  // brute force removal
+  player.ratings = "0";
+  // convert date
+  var year = player["Year_Born"];
+  var month = player["Month_Born"];
+  var day = player["Day_Born"];
+  player["Year_Born"] = "0";
+  player["Month_Born"] = "0";
+  player["Day_Born"] = "0";
+  player["Date of Birth"] = year + "-" + month + "-" + day; 
+  player["Age (roughly)"] = 2035 - year;
+
+  var retStat = "<table cellpadding='4'><col width='200'><col width='100'>";
+  for (var prop in player) {
+    if ( (player[prop] != "0") &&
+         (prop != "_id") &&
+         (prop != "coll") &&
+         (prop != "line") &&
+         (prop != "Record_Number") &&
+         (prop != "Drafted_By") &&
+         (prop != "College") &&
+         (prop != "Drafted_Position") &&
+         (prop != "Player_ID") &&
+         (prop.indexOf("SP") < 0) &&
+         (prop.indexOf("SY") < 0) &&
+         (prop.indexOf("Season_1") < 0) &&
+         (prop.indexOf("Home") < 0) ) {
+      var cleanProp = prop.replace(/_/g, " ");
+      retStat += "<tr><td>" + cleanProp + ":</td><td align='right'>" + player[prop] + "</td></tr>\n";
+    }
+  }
+  retStat += "</table>";
+  $( "#tab2" ).html(retStat);
+}
+
+function showInfo(id, name, pos, player) {
+  var tabs = $("<div><ul><li><a href='#tab1'>stats</a></li><li><a href='#tab2'>info</a></li></ul><div id='tab1'>Tab1 content</div><div id='tab2'>tab2 content</div></div>");
+  $( "#dialog" ).empty().append(tabs);
+  tabs.show();
+  tabs.tabs();
+  $("span.ui-dialog-title").text(pos + " " + name);
+  $( "#dialog" ).parent().css({position:"fixed"}).end().dialog("open");
+  stats(player);
+}
+
---
+++ usfl/routes/index.js
@@ -0,0 +1,128 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/usfl', function(req, res) {
+  res.redirect('/usfl/index.html');
+});
+
+router.get('/usfl/', function(req, res) {
+  res.redirect('/usfl/index.html');
+});
+
+router.get('/usfl/:page', function(req, res) {
+  var page = req.param('page');
+  console.log('page is ' + page);
+  if (page.indexOf('box') > -1) {
+    res.render('superbox', { 'page': page });
+  } else if (page.indexOf('jpg') > -1 ||
+             page.indexOf('gif') > -1 ||
+             page.indexOf('png') > -1) {
+    console.log('found graphics');
+    res.redirect('/box/' + page);
+  } else {
+    res.render('usfl/' + page);
+  }
+});
+
+// add the player ratings for the current season
+function showPlayer(req, res, player) {
+  var db = req.db;
+  var collection = db.get('player_ratings_season_2035');
+  var playerId = player['Player_ID'];
+  collection.findOne({'Player_ID': playerId}, {}, function(e, docs) {
+    docs.coll = 'player_ratings_season_2035';
+    player.ratings = docs;
+    res.send(JSON.stringify(player));
+  });
+}
+
+// get info on player with first and last
+// note that only one player info is ever returned
+router.get('/player/:first/:last', function(req, res) {
+  var db = req.db;
+  var collection = db.get('player_information');
+  var first = req.param('first');
+  var last = req.param('last');
+  console.log('looking for ' + first + ' ' + last);
+  collection.findOne({'First_Name': req.param('first'), 'Last_Name': req.param('last')}, {}, function(e, docs) {
+    console.log('this is found: ' + JSON.stringify(docs));
+    docs.coll = 'player_information';
+    showPlayer(req, res, docs);
+  });
+});
+
+// get a list of all players in player_information
+router.get('/player/', function(req, res) {
+    var db = req.db;
+    var collection = db.get('player_information');
+    collection.find({},{},function(e,docs){
+        var players = [];
+
+        docs.forEach(function(p) {
+          var player = {};
+          player.Position = p.Position;
+          player.First_Name = p.First_Name;
+          player.Last_Name = p.Last_Name;
+          players.push(player);
+        });
+        res.send(JSON.stringify(players));
+    });
+});
+
+router.get('/player/:year', function(req, res) {
+  var db = req.db;
+  var collection = db.get('player_ratings_season_' + req.param('year'));
+  collection.distinct('Player_ID', function(e, docs) {
+    res.send(JSON.stringify(docs));
+  });
+});
+
+router.get('/playerid/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('player_information');
+  var id = req.param('id');
+  collection.findOne({'Player_ID': id}, ['Player_ID', 'First_Name', 'Last_Name', 'Position'], function(e, docs) {
+    res.send(JSON.stringify(docs));
+  });
+});
+
+router.get('/playerid/year/:year', function(req, res) {
+  console.log('in playerid year');
+  var db = req.db;
+  var collection = db.get('player_ratings_season_' + req.param('year'));
+  collection.distinct('Player_ID', function(e, docs) {
+    console.log('docs is ' + docs);
+    var playerInfo = db.get('player_information');
+    // playerInfo.find({'Player_ID': '{$in: ' + docs + '}'}, ['Player_ID', 'First_Name', 'Last_Name', 'Position'], function(e, docs) {
+      // res.send(JSON.stringify(docs));
+    // });
+    var players = [];
+    docs.forEach(function(playerId) {
+      console.log('found player id ' + playerId);
+      var player = {};
+      playerInfo.findOne({'Player_ID': playerId}, ['Player_ID', 'First_Name', 'Last_Name', 'Position'], function(e, docs) {
+	players.push(docs);
+      });
+      players.push(player);
+    });
+    res.send(JSON.stringify(players));
+  });
+});
+
+// get position, first and last name of players in player_information
+router.get('/players/', function(req, res) {
+    var db = req.db;
+    var collection = db.get('player_information');
+    collection.find({},{},function(e,docs){
+        res.render('playerinfo', {
+            'playerinfo' : docs
+        });
+    });
+});
+
+module.exports = router;
