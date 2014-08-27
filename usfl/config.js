68636b98a35d2733560a6efa5fe9d35fd193d284
Torsten <tetet.nimitz@gmail.com>
2014-08-27 19:12:27 +0200

    Add APFL and TFL leagues to config

--- usfl/config.js
+++ usfl/config.js
@@ -9,5 +9,11 @@ config.leagues.ffl.year = "2040";
 config.leagues.usfl = {};
 config.leagues.usfl.week = "3";
 config.leagues.usfl.year = "2035";
+config.leagues.tfl = {};
+config.leagues.tfl.week = "9";
+config.leagues.tfl.year = "1994";
+config.leagues.apfl = {};
+config.leagues.apfl.week = "18";
+config.leagues.apfl.year = "2015";
 
 module.exports = config;
