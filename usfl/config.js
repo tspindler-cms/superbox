94168e53995058a4a1f5a9697fde46647a76296b
Torsten <tetet.nimitz@gmail.com>
2014-08-27 20:32:45 +0200

    Add logos

--- usfl/config.js
+++ usfl/config.js
@@ -9,6 +9,7 @@ config.leagues.apfl.name = "All Pro Football League";
 config.leagues.apfl.entry = "apfl";
 config.leagues.apfl.week = "18";
 config.leagues.apfl.year = "2015";
+config.leagues.apfl.logo = "/league/apfl/logo33.jpg";
 config.leagues.apfl.url = "http://www.fof-ffl.com/APFL/APFL.html";
 
 config.leagues.ffl = {};
@@ -16,6 +17,7 @@ config.leagues.ffl.name = "Franchise Football League";
 config.leagues.ffl.entry = "ffl";
 config.leagues.ffl.week = "6";
 config.leagues.ffl.year = "2040";
+config.leagues.ffl.logo = "/league/ffl/logo33.jpg";
 config.leagues.ffl.url = "http://fof-ffl.com/FFL/default.html";
 
 config.leagues.tfl = {};
@@ -23,6 +25,7 @@ config.leagues.tfl.name = "Throwback Football League";
 config.leagues.tfl.entry = "tfl";
 config.leagues.tfl.week = "9";
 config.leagues.tfl.year = "1994";
+config.leagues.tfl.logo = "/league/tfl/logo33.jpg";
 config.leagues.tfl.url = "http://www.fof-ffl.com/TFL/index.html";
 
 config.leagues.usfl = {};
@@ -30,6 +33,7 @@ config.leagues.usfl.name = "USFL";
 config.leagues.usfl.entry = "usfl";
 config.leagues.usfl.week = "3";
 config.leagues.usfl.year = "2035";
+config.leagues.usfl.logo = "/league/usfl/logo33.jpg";
 config.leagues.usfl.url = "http://www.fof-ffl.com/USFL/home.html";
 
 module.exports = config;
